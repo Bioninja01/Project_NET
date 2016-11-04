ea66f92c72407f6fcfea92ebdbda7fa1bb511778
James Vinson <vinsonjames02@gmail.com>
2016-11-04 09:03:30 -0400

    init

---
+++ Assets/Scripts/DialogSystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour {
+
+    public Text dialog;
+    public Image charater1;
+    public Image charater2;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+
+    public class DialogInterface {
+        public static DialogInterface instance;
+        private DialogInterface() { }
+        public static DialogInterface GetInstance() {
+            if(instance == null) {
+                instance = new DialogInterface();
+            }
+            return instance;
+        }
+    }
+}
